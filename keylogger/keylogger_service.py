3cbb72ba23df591fd9cead4faf476f994355f524
michael friedman <myklp1013@gmail.com>
2025-09-03 09:28:46 +0300

    add a new wey of keylogger

---
+++ keylogger/keylogger_service.py
@@ -0,0 +1,16 @@
+import keyboard
+
+class KeyloggerService:
+    def __init__(self,start_logging,stop_logging,get_logged_keys):
+        self.log = []
+
+    def start_logging(self):
+        
+
+    def stop_logging(self):
+        pass
+
+
+    def get_logged_keys(self):
+        pass
+
